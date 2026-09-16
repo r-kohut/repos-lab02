7b24b5b2bce29fb4e86f63e7a9a1f84616efc2fd
Roman Kohut <romankohut@MacBook-Pro-Roman.local>
2026-09-16 19:42:40 +0300

    Comment z1

--- Lab_02.cpp
+++ Lab_02.cpp
@@ -16,11 +16,11 @@ int main()
     cin  >>  b;
 
     z1 = (sqrt(2 * b + 2 * sqrt(b * b - 4))) / (sqrt (b * b-4) + b + 2);
-    z2 = (1.0) / (sqrt(b + 2));
+    //z2 = (1.0) / (sqrt(b + 2));
 
     cout << endl;
     cout << "Results of calculations: " << endl;
     cout << "z1 = " << z1 << endl;
-    cout << "z2 = " << z2 << endl;
+    // cout << "z2 = " << z2 << endl;
     return 0;
 }
