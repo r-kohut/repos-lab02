60dfee3dc1aa8a5f6767ad3d8989e37991c5b513
Roman Kohut <romankohut@MacBook-Pro-Roman.local>
2026-09-16 19:38:40 +0300

    Refine output

--- Lab_02.cpp
+++ Lab_02.cpp
@@ -12,13 +12,14 @@ int main()
     double z1; // результат обчислення 1-го виразу
     double z2; // результат обчислення 2-го виразу
 
-    cout << "b = ";
+    cout << " Please enter the value of b: ";
     cin  >>  b;
 
     z1 = (sqrt(2 * b + 2 * sqrt(b * b - 4))) / (sqrt (b * b-4) + b + 2);
     z2 = (1.0) / (sqrt(b + 2));
 
     cout << endl;
+    cout << "Results of calculations: " << endl;
     cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
     return 0;
